0c1ee45f4332309b8af29ca07070680b03bcef5e
Agnieszka Szkurłat <szkurlatagnieszka@gmail.com>
2026-03-09 21:16:17 +0100

    Add UserPayload class for user creation

--- src/test/java/payloads/UserPayload.java
+++ src/test/java/payloads/UserPayload.java
@@ -1,4 +1,12 @@
 package payloads;
 
 public class UserPayload {
+
+    public static String createUserPayload() {
+
+        return "{\n" +
+                "  \"name\": \"Anna\",\n" +
+                "  \"job\": \"QA Engineer\"\n" +
+                "}";
+    }
 }
