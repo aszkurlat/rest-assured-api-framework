94bb6534f9bca235cc80e9eb0fe473d68491db32
Agnieszka Szkurłat <szkurlatagnieszka@gmail.com>
2026-03-10 21:19:05 +0100

    feat: add basic ConfigLoader class and Properties object

--- src/test/java/utils/ConfigLoader.java
+++ src/test/java/utils/ConfigLoader.java
@@ -1,4 +1,19 @@
 package utils;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ConfigLoader {
+
+    static Properties prop = new Properties();
+
+    static {
+        try {
+            FileInputStream fis = new FileInputStream("config/config.properties");
+            prop.load(fis);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load config.properties", e);
+        }
+    }
 }
