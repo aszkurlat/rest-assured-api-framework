0cd8f782e9a5276c2fe750daa541ba7137158d31
Agnieszka Szkurłat <szkurlatagnieszka@gmail.com>
2026-03-11 21:57:07 +0100

    Add reusable method for converting response to JsonPath

--- src/test/java/utils/ReusableMethods.java
+++ src/test/java/utils/ReusableMethods.java
@@ -1,4 +1,14 @@
 package utils;
 
+import io.restassured.path.json.JsonPath;
+
 public class ReusableMethods {
+
+    public static JsonPath rawToJson(String response)
+    {
+        JsonPath js;
+        js = new JsonPath(response);
+        return js;
+    }
+
 }
